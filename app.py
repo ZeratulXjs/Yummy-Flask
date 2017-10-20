49edd47ddac1df41c94269585d537a9e42dee83a
Caesar Oweitu <oweitu@gmail.com>
2017-10-20 13:19:26 +0300

    Re-structure app Broken down templates to follow code more easily. Added WTForms validation in app file Re-structure yumRecipes

--- app.py
+++ app.py
@@ -1,23 +1,76 @@
-from flask import Flask, render_template, request
-from yumRecipes import *
+from flask import Flask, render_template, request, session, redirect, url_for, logging, flash 
+from wtforms import Form, StringField, PasswordField, TextAreaField, validators
+from passlib.hash import sha256_crypt
+from os import urandom
+from yumRecipes import Recipe, Users
 
 app = Flask(__name__)
-#users = users(uname, password)
 
+users = Users()
+user_info = {}
+
+# This is the index route that connects to the home view
 @app.route('/', methods=['POST','GET'])
-@app.route('/<uname>', methods=['GET', 'POST'])
-def userdash(uname=None): 
-    return render_template("index.html", uname=uname)
+def index(): 
+  return render_template("index.html", )
+
+# Define form attributes for signup using WTForms to allow easy validation
+class SignupForm(Form):
+  uname = StringField('Username', [validators.Length(min=3, max=50), validators.DataRequired()])
+  password = PasswordField('Password', [
+    validators.DataRequired(),
+    validators.Length(min=6),
+    validators.EqualTo('password_rep', message='Passwords do not match'),
+  ] )
+  password_rep = PasswordField('Repeat Password', [validators.DataRequired()])
 
 @app.route('/signup' , methods=['POST','GET'])
 def signup():
-    if request.method==['POST']:
-        
-        uname = request.form['uname']
-        password = request.form['password']
-        user = users(uname, password)
-    
-    return render_template("signup.html")
+    form = SignupForm(request.form)
+    if request.method == 'POST' and form.validate():
+      uname = form.uname.data
+      password = sha256_crypt.encrypt(str(form.password.data))
+
+      user_info[uname] = password      
+      
+      users.user_list.append(user_info)
+
+      flash('You have successfuly signed up!', 'success')
+
+      return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
+
+
+# This is the login route that connects to the login view
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+      uname = request.form['uname']
+      password_candidate = request.form['password']
+      
+      for item in users.user_list:
+        if item[uname] == uname:
+          password_real = item.get[uname]                  
+          if sha256_crypt.verify(password_candidate, password_real):
+            app.logger.info("Login Success")
+
+      app.logger.info("Username doesn't exist")
+          
+    return render_template("login.html")
+
+
+
+my_recipes = Recipe()
+nu_recipes = my_recipes.add_recipes()
+@app.route('/recipes', methods = ['POST', 'GET'])
+def recipes():
+  return render_template("recipes.html", nu_recipes = nu_recipes)
+
+@app.route('/recipe/<title>', methods = ['POST', 'GET'])
+def recipe(title):
+  return render_template("recipes.html", title = title)
+
 
 if __name__ == "__main__":
+    app.secret_key = urandom(32)
     app.run(debug=True)
--- yumRecipes.py
+++ yumRecipes.py
@@ -1,80 +1,34 @@
 # user accounts will be derived from this class 
 
-class users(object):
-    def __init__(self, uname, password):
-        regusers = {}
+class Users(object):
+  def __init__(self):
+    self.user_list = []
 
-
-class new_user(object):
-    def __init__(self, uname, password):
-        self.uname = str(uname)
-        self.password = str(password)
-        
-    def signup(self, uname, password):
-        #Checking that the uname and password are not empty  
-        if not uname or password:
-            print "Please enter a username or password"
-        elif key in regusers.keys():            
-            print "Please enter a unique username"
-        else:
-            regusers.update({'uname':password})
-
-    def login(self, uname, password):
-        if not uname or password:
-            print "Please enter a username or password"
-        elif uname not in regusers.keys():
-            print "User does not exist"   
-
-        elif uname in regusers.keys():
-            if regusers[key] == password:
-                print "You're in"
-                    #Link to user's recipe dictionaries(categories and recipes)
-    
-
-
-# Recipe categories will be set by this class   
-class RecipeCategory(object):
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-        recipe_cats = {}
-
-    def del_cat(self, cat):
-        if cat in recipe_cats:
-            del recipe_cats[cat]
-            print 'Recipe Category deleted'
-        else:
-            print 'Category does not exits'
-
-    def edit_name(self, category_name):
-        
-        if key in regusers.keys():
-                print "Please enter a unique username"
-        else:
-            regusers.update({'uname':password})
-       
-
-    def edit_description(self, description):
-        pass
-    def get_recipes(self):
-        pass
-    def add_recipe(self):
-        pass
 # Creating the recipe class where the individual recipes will be held. 
 
 class Recipe(object):
-    def __init__(self, name, description = [], newstep = "", oldstep = ""):
-        self.name = name
-        self.description = description
-        self.newstep = newstep
-        self.oldstep = oldstep
-        
-    def __del__(self):
-        print 'Deleting recipe'
+  def __init__(self):
+    self.recipes = []
 
-    def add_step(self, newstep):
-            self.description.append(newstep)
-            return self.description
-    def del_step(self, oldstep):
-            self.description.remove(oldstep)
-            return self.description   
+  def add_recipes(self):
+    recipes = [
+      {
+          'id' : 1,
+          'title' : 'jacket potatoes',
+          'body' : 'Wash potatoes, boil with jackets on for 10min, apply some butter, wrap in alu-foil and bake for 15min',
+          'author' : 'Caesar'
+      },
+      {
+          'id' : 2,
+          'title' : 'mash potatoes',
+          'body' : 'Wash potatoes, boil with jackets on for 10min, add some butter and milk, mash and serve',
+          'author' : 'Caesar'
+      },
+      {
+          'id' : 3,
+          'title' : 'deep fried potatoes',
+          'body' : 'Wash potatoes, boil with jackets on for 10min, deep fry',
+          'author' : 'Caesar'
+      }
+    ]
+    return recipes
